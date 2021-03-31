adedc891d3fecf95ccc9f4a9927b8a9ecb37a782
THOMAS SUBLET <thomas.sublet@gmail.com>
2021-03-31 12:43:48 +0200

    Create User v1.0

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Mime\Email as MimeEmail;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\EqualTo;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new MimeEmail()
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 8
+                    ])
+                ]
+            ])
+            ->add('check_password', PasswordType::class, [
+                'constraints' => [
+                    new EqualTo([
+                        'propertyPath' => 'password'
+                    ])
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
+            ->add('adresse', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
+            ->add('certificat', FileType::class, [
+                'label' => 'Certificat d\'autoritsation',
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'certificat/jpeg',
+                            'certificat/png',
+                            'certificat/pdf',
+                        ]
+                    
+                    ])
+                ],
+                'attr' => [
+                    'class' => 'form-field',
+                    'placeholder' => 'Fichier justificatif'
+                    ]
+            ] )
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer un compte',
+                'attr' => [
+                    'class' => 'button'
+                    ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
